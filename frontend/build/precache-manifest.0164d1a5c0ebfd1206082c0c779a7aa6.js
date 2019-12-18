99475a5744fe6273a88656dd1423585991cc99d8
jcazorla2000 <jose.cazorla00@gmail.com>
2019-12-18 16:49:47 -0600

    predeploy

---
+++ frontend/build/precache-manifest.0164d1a5c0ebfd1206082c0c779a7aa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59e8cf90f2ab62edd5e25088f50d574",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3b5ee4e5558206e6136",
+    "url": "/static/css/2.38efd7c1.chunk.css"
+  },
+  {
+    "revision": "af1bd10312e1ea87bceb",
+    "url": "/static/css/main.d3aabb37.chunk.css"
+  },
+  {
+    "revision": "b3b5ee4e5558206e6136",
+    "url": "/static/js/2.90c38864.chunk.js"
+  },
+  {
+    "revision": "af1bd10312e1ea87bceb",
+    "url": "/static/js/main.b556f46b.chunk.js"
+  },
+  {
+    "revision": "80f95f4d0fbbd034142a",
+    "url": "/static/js/runtime-main.6b303a61.js"
+  }
+]);

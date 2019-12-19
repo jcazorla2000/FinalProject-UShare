41783f22fcb365f4edf363841e0b8dac36af03e9
jcazorla2000 <jose.cazorla00@gmail.com>
2019-12-19 10:37:41 -0600

    deploy4

--- backend/app.js
+++ backend/app.js
@@ -20,13 +20,26 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: [process.env.FRONTENDPOINT],
-    preflightContinue: true
-  })
-);
+
+
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://modest-mcnulty-d19b85.netlify.com/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.use(
   session({
---
+++ frontend/build/precache-manifest.3d4314d207e885a6658bf5f67513daa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7afbf9d32df897ec98d626f1c08f32ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09b9fa776cbb6a74d492",
+    "url": "/static/css/2.38efd7c1.chunk.css"
+  },
+  {
+    "revision": "e2ec7769b1f714eb0a47",
+    "url": "/static/css/main.d3aabb37.chunk.css"
+  },
+  {
+    "revision": "09b9fa776cbb6a74d492",
+    "url": "/static/js/2.0d77ddda.chunk.js"
+  },
+  {
+    "revision": "e2ec7769b1f714eb0a47",
+    "url": "/static/js/main.52206ad5.chunk.js"
+  },
+  {
+    "revision": "80f95f4d0fbbd034142a",
+    "url": "/static/js/runtime-main.6b303a61.js"
+  }
+]);

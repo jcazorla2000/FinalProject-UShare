4996060234f882112f7e06f0b66566575f038d70
jcazorla2000 <jose.cazorla00@gmail.com>
2019-12-19 10:41:07 -0600

    deploy5

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://modest-mcnulty-d19b85.netlify.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://modest-mcnulty-d19b85.netlify.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
